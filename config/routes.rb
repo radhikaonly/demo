b0a1f2ced099da1ac16709ef01528250e9c25e02
Radhika Ramnauth <Radhikaonly@hotmail.com>
2016-02-10 22:09:04 -1200

    created links and route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
    root 'pages#home'
 
+   get "about" => "pages#about"
+
+   get "ex" => "pages#ex"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
